4ef8a91218ef22fef8b52b3e117a7fd64c54bfe9
immarylim <skyhjs0629@icloud.com>
2023-09-29 23:18:17 +0900

    complete :) !

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'My Pig ..',
+  imageUrl: 'https://i.namu.wiki/i/-YK8L4RsjhgIhps7bNF89BpHg4TP48JSc6W98osE5o_bdxfZ0ZD0rXAXJz-_MmGMiLPJLwRHqz1tb-wROcSONw.webp',
+  imageSize: 90,
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-box">
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </div>
     </div>
   );
 }
